0fb7f438c13b5012f6feb4b82855623612e8a4b1
Ashalu <developer.aashish02@gmail.com>
2019-02-25 16:46:15 +0530

    optimized code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,6 @@
 class UsersController < ApplicationController
   def index
   	@user = User.all.order(pi: :asc)
-  	@arr = []
-  	@arr1 = []
-  	@arr2 = []
-  	@arr3 = []
-  	@h = Hash.new 
-  	@user.each do |name|
-		  	if !@arr.to_set.include?(name.pi) 
-							@arr.push(name.pi)	
-				end		
-	  end
-	  c = (@arr.last - @arr.first)
-	  a = (c.to_f/10)
-	  a = a.round(2)
-	  @arr1.push(@arr[0])
-	  i = 1;
-	  while i<= 10
-	  	@arr1.push((@arr1[i-1] + a).round(2))
-	  	@value = User.all.where("pi >= ? and pi <= ?", @arr1[i-1],@arr1[i]).count
-	  	x = @arr1[i-1].to_s + '-' + @arr1[i].to_s
-	  	@arr3.push(x)
-	  	@arr2.push(@value)
-	  	i +=1
-	  end
-	  j = 0;
-	  while j<=10
-	  	@h[@arr3[j]] = @arr2[j]
-	  	j += 1;
-	  end
-	end
+  	@hash_of_data = User.common_data @user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ApplicationRecord
 	self.inheritance_column = nil
 
 	enum type: [:abc, :xyz]
+
+	def self.common_data user
+		@arr = []
+  	@arr1 = []
+  	@arr2 = []
+  	@arr3 = []
+  	@h = Hash.new 
+  	user.each do |name|
+		  	if !@arr.to_set.include?(name.pi) 
+							@arr.push(name.pi)	
+				end		
+	  end
+	  c = (@arr.last - @arr.first)
+	  a = (c.to_f/10)
+	  a = a.round(2)
+	  @arr1.push(@arr[0])
+	  i = 1;
+	  while i<= 10
+	  	@arr1.push((@arr1[i-1] + a).round(2))
+	  	@value = User.all.where("pi >= ? and pi <= ?", @arr1[i-1],@arr1[i]).count
+	  	x = @arr1[i-1].to_s + '-' + @arr1[i].to_s
+	  	@arr3.push(x)
+	  	@arr2.push(@value)
+	  	i +=1
+	  end
+	  j = 0;
+	  while j<=10
+	  	@h[@arr3[j]] = @arr2[j]
+	  	j += 1;
+	  end
+
+	  return @h
+	end
 end
